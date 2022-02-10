4cd6237d62af558761a98dfc78f2e1f6ce30947a
Noemi Andras <noemiandras22@gmail.com>
2022-02-10 17:25:23 -0600

    implement pseudo RNG

--- benfNum.py
+++ benfNum.py
@@ -2,7 +2,16 @@
 Author: Noemi Andras
 Project Title: Benford's Law Applied on RNG (Random Number Generation)
 '''
+from random import randrange
+
+
+# pseudorandom number generation
+def pseudo_rand_num(size):
+    mill = 1000000
+    return [randrange(mill) for i in range(size)]
+
 
 if __name__ == "__main__":
     print("Hello World")
-
+    print("Random values generated: \n")
+    print(pseudo_rand_num(100))
