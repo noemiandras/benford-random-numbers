63e398530e1c26594db683d9b2059f5a3bfdcaf4
Noemi Andras <noemiandras22@gmail.com>
2022-02-10 16:42:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Benford's Law Applied on RNG
+RNG = Random Number Generation 
+
+To run project:
+```bash
+py benfNum.py
+```
---
+++ benfNum.py
@@ -0,0 +1,8 @@
+'''
+Author: Noemi Andras
+Project Title: Benford's Law Applied on RNG (Random Number Generation)
+'''
+
+if __name__ == "__main__":
+    print("Hello World")
+
